cac6b4e2a9e877a3cb8d6f3decd565374bbf7aa1
Mai Pham <maitphm@gmail.com>
2021-01-14 21:26:53 -0500

    commit tree a178326b82d5231cef967fe2bb61273ad681962a

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
 
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
